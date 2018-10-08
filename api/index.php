7c995a40c10bd74545e49d20a35fc9b65bf824c3
Memo Cachú <gcachu.o@gmail.com>
2018-10-08 01:59:52 -0500

    libs

--- api/index.php
+++ api/index.php
@@ -6,13 +6,19 @@
  * Time: 09:28 PM
  */
 
+$version = '181008';
+
+require "libs/system.php";
+
 setcookie('XDEBUG_SESSION', 'PHPSTORM');
 ini_set('display_errors', 1);
 set_error_handler('error_handler');
 register_shutdown_function('shutdown_function');
 
 try {
+
     $action = isset_get($_REQUEST['action']);
+
     /** @var array|string|null $response */
     if (empty($action)) {
         $response = null;
@@ -31,40 +37,4 @@ try {
     die(json_encode(['response' => 'App Error', 'code' => http_response_code(), 'error' => compact('errno', 'errstr', 'errfile', 'errline')]));
 }
 
-echo json_encode(['response' => $GLOBALS['response'], 'code' => http_response_code(), 'version' => '181008']);
-
-/**
- * @param $variable
- * @return string
- */
-function isset_get(&$variable)
-{
-    $variable = isset($variable) ? $variable : '';
-    return $variable;
-}
-
-/**
- * @param string $error
- * @param int $code
- * @throws Exception
- */
-function set_error($error, $code = 0)
-{
-    throw new Exception($error, $code);
-}
-
-/**
- * @param int $errno
- * @param string $errstr
- * @param string $errfile
- * @param int $errline
- */
-function error_handler($errno, $errstr, $errfile, $errline)
-{
-    http_response_code(500);
-    die(json_encode(['response' => 'System Error', 'code' => http_response_code(), 'error' => compact('errno', 'errstr', 'errfile', 'errline')]));
-}
-
-function shutdown_function()
-{
-}
+echo json_encode(['response' => $response, 'code' => http_response_code(), 'version' => $version]);
---
+++ api/libs/system.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cachu
+ * Date: 8/10/18
+ * Time: 01:57 AM
+ */
+
+/**
+ * @param $variable
+ * @return string
+ */
+function isset_get(&$variable)
+{
+    $variable = isset($variable) ? $variable : '';
+    return $variable;
+}
+
+/**
+ * @param string $error
+ * @param int $code
+ * @throws Exception
+ */
+function set_error($error, $code = 0)
+{
+    throw new Exception($error, $code);
+}
+
+/**
+ * @param int $errno
+ * @param string $errstr
+ * @param string $errfile
+ * @param int $errline
+ */
+function error_handler($errno, $errstr, $errfile, $errline)
+{
+    http_response_code(500);
+    die(json_encode(['response' => 'System Error', 'code' => http_response_code(), 'error' => compact('errno', 'errstr', 'errfile', 'errline')]));
+}
+
+function shutdown_function()
+{
+}
