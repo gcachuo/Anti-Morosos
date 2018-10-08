5e88e8bb414c0d89ee1f8996a9653a9ad7b12801
Memo Cachú <gcachu.o@gmail.com>
2018-10-08 00:56:46 -0500

    init api

--- api/index.php
+++ api/index.php
@@ -6,3 +6,17 @@
  * Time: 09:28 PM
  */
 
+setcookie('XDEBUG_SESSION', 'PHPSTORM');
+ini_set('display_errors', 1);
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+/** @var array|string|null $response */
+if (function_exists($action)) {
+    $response = $action();
+} else {
+    http_response_code(500);
+    $response = 'Error (1000).';
+}
+
+ob_clean();
+echo json_encode(['response' => $response, 'code' => http_response_code(), 'version' => '181008']);
