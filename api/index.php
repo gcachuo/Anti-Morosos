dae4758a4525546757d9f2442f3e22b64f6f7c60
Memo Cachú <gcachu.o@gmail.com>
2018-10-08 01:54:00 -0500

    api index

--- api/index.php
+++ api/index.php
@@ -8,15 +8,63 @@
 
 setcookie('XDEBUG_SESSION', 'PHPSTORM');
 ini_set('display_errors', 1);
-$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+set_error_handler('error_handler');
+register_shutdown_function('shutdown_function');
 
-/** @var array|string|null $response */
-if (function_exists($action)) {
-    $response = $action();
-} else {
+try {
+    $action = isset_get($_REQUEST['action']);
+    /** @var array|string|null $response */
+    if (empty($action)) {
+        $response = null;
+    } elseif (function_exists($action)) {
+        $response = $action();
+    } else {
+        set_error("The function does not exists. ($action)");
+    }
+} catch (Exception $exception) {
     http_response_code(500);
-    $response = 'Error (1000).';
+    $errno = $exception->getCode();
+    $errstr = $exception->getMessage();
+    $errfile = $exception->getFile();
+    $errline = $exception->getLine();
+
+    die(json_encode(['response' => 'App Error', 'code' => http_response_code(), 'error' => compact('errno', 'errstr', 'errfile', 'errline')]));
 }
 
-ob_clean();
-echo json_encode(['response' => $response, 'code' => http_response_code(), 'version' => '181008']);
+echo json_encode(['response' => $GLOBALS['response'], 'code' => http_response_code(), 'version' => '181008']);
+
+/**
+ * @param $variable
+ * @return string
+ */
+function isset_get(&$variable)
+{
+    $variable = isset($variable) ? $variable : '';
+    return $variable;
+}
+
+/**
+ * @param string $error
+ * @param int $code
+ * @throws Exception
+ */
+function set_error($error, $code = 0)
+{
+    throw new Exception($error, $code);
+}
+
+/**
+ * @param int $errno
+ * @param string $errstr
+ * @param string $errfile
+ * @param int $errline
+ */
+function error_handler($errno, $errstr, $errfile, $errline)
+{
+    http_response_code(500);
+    die(json_encode(['response' => 'System Error', 'code' => http_response_code(), 'error' => compact('errno', 'errstr', 'errfile', 'errline')]));
+}
+
+function shutdown_function()
+{
+}
