6142fc23aa807acba945cc1e6713d0cfbb724c71
Memo Cachú <gcachu.o@gmail.com>
2018-11-13 01:07:24 -0600

    webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,14 @@
+require('jquery');
+require('bootstrap');
+Mustache = require('mustache');
+require('@fortawesome/fontawesome-pro/scss/fontawesome.scss');
+require('bootstrap/scss/bootstrap.scss');
+
+require('./js/system');
+require('./js/dashboard');
+require('./js/pending-validation');
+require('./js/profile');
+require('./js/sign-in');
+require('./js/sign-up');
+
+require('./css/project.scss');
