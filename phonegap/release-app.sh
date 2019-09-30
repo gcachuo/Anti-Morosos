d6908a27c37b809aae92dc1bcf49d2297143399a
Memo Cachu <gcachu.o@gmail.com>
2019-09-30 16:51:23 -0500

    build 1.1.0

--- phonegap/release-app.sh
+++ phonegap/release-app.sh
@@ -11,4 +11,4 @@ phonegap build android --release >/dev/null
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore build/android/android.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk android-app-key
 rm platforms/android/app/build/outputs/apk/release/app-release.apk
 zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk platforms/android/app/build/outputs/apk/release/app-release.apk
-rm -f build/android/release/*.apk && cp platforms/android/app/build/outputs/apk/release/app-release.apk build/android/${appname}".apk"
+rm -f build/android/*.apk && cp platforms/android/app/build/outputs/apk/release/app-release.apk build/android/${appname}".apk"
